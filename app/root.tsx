51d60d4b5bf2c1898b0419a435c538dad0328246
Javier Valverde <xagot.developer@gmail.com>
2024-04-13 09:37:32 -0500

    Managing the History Stack

--- app/root.tsx
+++ app/root.tsx
@@ -63,7 +63,13 @@ export default function App() {
           <div>
             <Form
               id="search-form"
-              onChange={(event) => submit(event.currentTarget)}
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                console.log(isFirstSearch);
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
               role="search"
             >
               <input
