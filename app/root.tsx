8e865cfc45d898e5a56af2f98cc016ce7183d146
Javier Valverde <xagot.developer@gmail.com>
2024-04-13 00:01:20 -0500

    URLSearchParams and GET Submissions

--- app/root.tsx
+++ app/root.tsx
@@ -11,13 +11,15 @@ import {
   useLoaderData,
   useNavigation,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { createEmptyContact, getContacts } from "./data";
 import appStylesHref from "./app.css?url";
 
 // existing exports
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
